16bffc0ab91a5a7c8e0a844bc6b93e2713c29a91
Xavier Olivares <xm.olivares@gmail.com>
2020-04-17 19:43:57 -0400

    new practice with blade library

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>Welcome to the <?= $name; ?></h1>
+    <ul>
+    @foreach ($tasks as $task)
+        <li>{{ $task }}</li>
+    @endforeach
+    </ul>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $name = 'Jungle';
+    $tasks = [
+        'Go to the store',
+        'Finish my screencast',
+        'Clean the house'
+    ];
+    return view('welcome', compact('tasks', 'name'));
+});
