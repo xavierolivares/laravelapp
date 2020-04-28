3b04c059dc2aa554354c0cc91ff1cef87e96c0b4
Xavier Olivares <xm.olivares@gmail.com>
2020-04-27 20:13:39 -0400

    slight edits to email response

--- resources/views/emails/contactus.blade.php
+++ resources/views/emails/contactus.blade.php
@@ -1,8 +1,8 @@
-<h2>Hello Admin,</h2>
-<p>You received an email from : {{ $name }}</p>
-<p>Here are the details:</p>
+<h1>freelancingweb.dev</h1>
+<p>Hello Admin, you received an email from : {{ $name }}</p>
+<p>Here are their details:</p>
 <p>Name: {{ $name }}</p>
 <p>Email: {{ $email }}</p>
 <p>Subject: {{ $subject }}</p>
 <p>Message: {{ $user_message }}</p>
-<p>Thank You</p>
+<p>Thank You!</p>
