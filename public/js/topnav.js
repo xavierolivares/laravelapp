7ae616143e0a3bcd0dd40aaa76447c74c13690ba
Xavier Olivares <xm.olivares@gmail.com>
2020-04-28 18:23:01 -0400

    refactored master blade template

---
+++ public/js/topnav.js
@@ -0,0 +1,15 @@
+function clickTopNav() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function closeTopNav() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+}
