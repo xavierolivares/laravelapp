22a62d41f1c0088716c4a423189c8e027a16eddc
Xavier Olivares <xm.olivares@gmail.com>
2020-04-28 19:41:57 -0400

    mobile responsive styling changes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -38,14 +38,12 @@
             </div>
         </div>
         <div class="footer links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://vapor.laravel.com">Vapor</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div>
+                <a href="/skills">Skills</a>
+                <a href="/projects">Projects</a>
+                <a href="/contact">Contact</a>
+                <a href="/about">About</a>
+            </div>
         </div>
     </body>
 </html>
