0eb51f34b4e2c07416e8e3623f36bac147e4804d
Xavier Olivares <xm.olivares@gmail.com>
2020-04-28 20:18:22 -0400

    added checkmark confirmation to contact form submission

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -40,8 +40,11 @@
                         </span>
                     @endif
                 </div>
-            <div class="form-group">
+            <div class="form-group submit-group">
                 <input id="submit" type="submit" name="btnSubmit" class="btn btn-primary btn-round btn-sm" value="Send Message" />  
+                @if (Session::has('success'))
+                    <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+                @endif
             </div>
         </div>
     </form>
